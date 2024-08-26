99b43aaaf30ac7f0fd4ecca1b35e2d8f32337a17
Piyal Guho Dipto <diptoguho4@gmail.com>
2024-08-27 01:19:52 +0600

    first commit

---
+++ resources/views/admin/partials/assets/css.blade.php
@@ -0,0 +1,19 @@
+<link rel="shortcut icon" type="image/x-icon"
+        href="https://dreamspos.dreamstechnologies.com/html/template/assets/img/favicon.png">
+
+    <link rel="stylesheet" href="admin/assets/css/bootstrap.min.css">
+
+    <link rel="stylesheet"
+        href="assets/css/bootstrap-datetimepicker.min.css">
+
+    <link rel="stylesheet" href="admin/assets/css/animate.css">
+
+    <link rel="stylesheet"
+        href="admin/assets/plugins/select2/css/select2.min.css">
+
+    <link rel="stylesheet"
+        href="admin/assets/plugins/fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet"
+        href="admin/assets/plugins/fontawesome/css/all.min.css">
+
+    <link rel="stylesheet" href="admin/assets/css/style.css">
---
+++ resources/views/admin/partials/assets/js.blade.php
@@ -0,0 +1,33 @@
+<script src="admin/assets/js/jquery-3.7.1.min.js"
+        type="2c8d1b9e5f6705bf4267f265-text/javascript"></script>
+
+    <script src="admin/assets/js/feather.min.js"
+        type="2c8d1b9e5f6705bf4267f265-text/javascript"></script>
+
+    <script src="admin/assets/js/jquery.slimscroll.min.js"
+        type="2c8d1b9e5f6705bf4267f265-text/javascript"></script>
+
+    <script src="admin/assets/js/bootstrap.bundle.min.js"
+        type="2c8d1b9e5f6705bf4267f265-text/javascript"></script>
+
+    <script src="admin/assets/plugins/apexchart/apexcharts.min.js"
+        type="2c8d1b9e5f6705bf4267f265-text/javascript"></script>
+    <script src="admin/assets/plugins/apexchart/chart-data.js"
+        type="2c8d1b9e5f6705bf4267f265-text/javascript"></script>
+
+    <script
+        src="admin/assets/plugins/sweetalert/sweetalert2.all.min.js"
+        type="2c8d1b9e5f6705bf4267f265-text/javascript"></script>
+    <script src="admin/assets/plugins/sweetalert/sweetalerts.min.js"
+        type="2c8d1b9e5f6705bf4267f265-text/javascript"></script>
+
+    <script src="admin/assets/js/theme-script.js"
+        type="2c8d1b9e5f6705bf4267f265-text/javascript"></script>
+    <script src="admin/assets/js/script.js"
+        type="2c8d1b9e5f6705bf4267f265-text/javascript"></script>
+    <!-- <script
+        src="https://dreamspos.dreamstechnologies.com/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js"
+        data-cf-settings="2c8d1b9e5f6705bf4267f265-|49" defer></script> -->
+        <script
+        src="admin/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js"
+        data-cf-settings="2c8d1b9e5f6705bf4267f265-|49" defer></script>

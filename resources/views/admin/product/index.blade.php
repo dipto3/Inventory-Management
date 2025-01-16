7f6bdd6d7d60d772dab73df917214e95fe4ef832
dipto <diptoguho4@gmail.com>
2025-01-16 14:19:32 +0600

    product page design

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -1,58 +1,57 @@
 @extends('admin.layouts.master')
 @section('admin.content')
-<div class="main-content">
+    <div class="main-content">
 
-    <div class="page-content">
-        <div class="container-fluid">
-            <!-- start page title -->
-            <div class="row">
-                <div class="col-12">
-                    <div
-                        class="page-title-box d-sm-flex align-items-center justify-content-between bg-galaxy-transparent">
-                        <h4 class="mb-sm-0">Categories</h4>
+        <div class="page-content">
+            <div class="container-fluid">
+                <!-- start page title -->
+                <div class="row">
+                    <div class="col-12">
+                        <div
+                            class="page-title-box d-sm-flex align-items-center justify-content-between bg-galaxy-transparent">
+                            <h4 class="mb-sm-0">Products</h4>
 
-                        <div class="page-title-right">
-                            <ol class="breadcrumb m-0">
-                                <li class="breadcrumb-item"><a href="javascript: void(0);">Inventory</a></li>
-                                <li class="breadcrumb-item active">Category</li>
-                            </ol>
+                            <div class="page-title-right">
+                                <ol class="breadcrumb m-0">
+                                    <li class="breadcrumb-item"><a href="javascript: void(0);">Inventory</a></li>
+                                    <li class="breadcrumb-item active">Products</li>
+                                </ol>
+                            </div>
+
                         </div>
-
                     </div>
                 </div>
-            </div>
 
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4 class="card-title mb-0">Add, Edit & Remove</h4>
-                        </div><!-- end card header -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4 class="card-title mb-0">Add, Edit & Remove</h4>
+                            </div><!-- end card header -->
 
-                        <div class="card-body">
-                            <div class="listjs-table" id="customerList">
-                                <div class="row g-4 mb-3">
-                                    <div class="col-sm-auto">
-                                        <div>
-                                            <a class="btn btn-success add-btn" href="{{ route('product.create') }}"
-                                                ><i
-                                                    class="ri-add-line align-bottom me-1"></i> Add</a>
+                            <div class="card-body">
+                                <div class="listjs-table" id="customerList">
+                                    <div class="row g-4 mb-3">
+                                        <div class="col-sm-auto">
+                                            <div>
+                                                <a class="btn btn-success add-btn" href="{{ route('product.create') }}"><i
+                                                        class="ri-add-line align-bottom me-1"></i> Add</a>
 
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="col-sm">
-                                        <div class="d-flex justify-content-sm-end">
-                                            <div class="search-box ms-2">
-                                                <input type="text" class="form-control search"
-                                                    placeholder="Search...">
-                                                <i class="ri-search-line search-icon"></i>
+                                        <div class="col-sm">
+                                            <div class="d-flex justify-content-sm-end">
+                                                <div class="search-box ms-2">
+                                                    <input type="text" class="form-control search"
+                                                        placeholder="Search...">
+                                                    <i class="ri-search-line search-icon"></i>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
 
-                                <div class="table-responsive table-card mt-3 mb-1">
-                                    {{-- <table class="table align-middle table-nowrap" id="customerTable">
+                                    <div class="table-responsive table-card mt-3 mb-1">
+                                        {{-- <table class="table align-middle table-nowrap" id="customerTable">
                                         <thead class="table-light">
                                             <tr>
                                                 <th scope="col" style="width: 50px;">
@@ -119,36 +118,36 @@
 
                                         </tbody>
                                     </table> --}}
-                                    <div class="noresult" style="display: none">
-                                        <div class="text-center">
-                                            <lord-icon src="https://cdn.lordicon.com/msoeawqm.json" trigger="loop"
-                                                colors="primary:#121331,secondary:#08a88a"
-                                                style="width:75px;height:75px"></lord-icon>
-                                            <h5 class="mt-2">Sorry! No Result Found</h5>
-                                            {{-- <p class="text-muted mb-0">We've searched more than 150+ Orders We did not find any orders for you search.</p> --}}
+                                        <div class="noresult" style="display: none">
+                                            <div class="text-center">
+                                                <lord-icon src="https://cdn.lordicon.com/msoeawqm.json" trigger="loop"
+                                                    colors="primary:#121331,secondary:#08a88a"
+                                                    style="width:75px;height:75px"></lord-icon>
+                                                <h5 class="mt-2">Sorry! No Result Found</h5>
+                                                {{-- <p class="text-muted mb-0">We've searched more than 150+ Orders We did not find any orders for you search.</p> --}}
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="d-flex justify-content-end">
+                                        <div class="pagination-wrap hstack gap-2">
+                                            <a class="page-item pagination-prev disabled" href="javascript:void(0);">
+                                                Previous
+                                            </a>
+                                            <ul class="pagination listjs-pagination mb-0"></ul>
+                                            <a class="page-item pagination-next" href="javascript:void(0);">
+                                                Next
+                                            </a>
                                         </div>
                                     </div>
                                 </div>
-
-                                <div class="d-flex justify-content-end">
-                                    <div class="pagination-wrap hstack gap-2">
-                                        <a class="page-item pagination-prev disabled" href="javascript:void(0);">
-                                            Previous
-                                        </a>
-                                        <ul class="pagination listjs-pagination mb-0"></ul>
-                                        <a class="page-item pagination-next" href="javascript:void(0);">
-                                            Next
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div><!-- end card -->
+                            </div><!-- end card -->
+                        </div>
+                        <!-- end col -->
                     </div>
                     <!-- end col -->
                 </div>
-                <!-- end col -->
             </div>
+            <!-- container-fluid -->
         </div>
-        <!-- container-fluid -->
-    </div>
-@endsection
+    @endsection

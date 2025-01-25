40564c9f7ca69106d53235c5d72a5d3d675eee8e
Anannya Roy <roy12anna@gmail.com>
2025-01-25 14:53:07 +0600

    category

--- database/migrations/2024_08_27_063507_create_categories_table.php
+++ database/migrations/2024_08_27_063507_create_categories_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->integer('parent_id');
+            $table->string('slug');
+            $table->integer('ordering');
             $table->text('description')->nullable();
             $table->tinyInteger('status');
             $table->timestamps();

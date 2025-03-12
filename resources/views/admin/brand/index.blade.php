3766e797d39ab43c933713d93aa244497251a344
dipto <diptoguho4@gmail.com>
2025-03-13 02:29:21 +0600

    brands data table

--- resources/views/admin/brand/index.blade.php
+++ resources/views/admin/brand/index.blade.php
@@ -9,12 +9,12 @@
                     <div class="col-12">
                         <div
                             class="page-title-box d-sm-flex align-items-center justify-content-between bg-galaxy-transparent">
-                            <h4 class="mb-sm-0">Brands</h4>
+                            <h4 class="mb-sm-0">Supplier</h4>
 
                             <div class="page-title-right">
                                 <ol class="breadcrumb m-0">
                                     <li class="breadcrumb-item"><a href="javascript: void(0);">Inventory</a></li>
-                                    <li class="breadcrumb-item active">Brands</li>
+                                    <li class="breadcrumb-item active">supplier</li>
                                 </ol>
                             </div>
 
@@ -27,61 +27,31 @@
                         <div class="card">
                             <div class="card-header">
                                 <h4 class="card-title mb-0">Add, Edit & Remove</h4>
+                                <div class="d-flex justify-content-sm-end">
+                                    <button type="button" class="btn btn-success add-btn" data-bs-toggle="modal"
+                                        id="create-btn" data-bs-target="#showBrandModal"><i
+                                            class="ri-add-line align-bottom me-1"></i> Create</button>
+                                </div>
                             </div><!-- end card header -->
 
                             <div class="card-body">
                                 <div class="listjs-table" id="customerList">
-                                    <div class="row g-4 mb-3">
-                                        <div class="col-sm-auto">
-                                            <div>
-                                                <button type="button" class="btn btn-success add-btn"
-                                                     id="create-btn" data-bs-toggle="modal" data-bs-target="#showBrandModal"><i
-                                                        class="ri-add-line align-bottom me-1"></i> Add</button>
-
-                                            </div>
-                                        </div>
-                                        <div class="col-sm">
-                                            <div class="d-flex justify-content-sm-end">
-                                                <div class="search-box ms-2">
-                                                    <input type="text" class="form-control search"
-                                                        placeholder="Search...">
-                                                    <i class="ri-search-line search-icon"></i>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="table-responsive table-card mt-3 mb-1">
-                                        <table class="table align-middle table-nowrap" id="customerTable">
-                                            <thead class="table-light">
+                                    <div class="table-responsive">
+                                        <table class="table table-striped" id="example">
+                                            <thead>
                                                 <tr>
-                                                    <th scope="col" style="width: 50px;">
-                                                        <div class="form-check">
-                                                            <input class="form-check-input" type="checkbox" id="checkAll"
-                                                                value="option">
-                                                        </div>
-                                                    </th>
-                                                    <th class="sort" data-sort="title">Title</th>
-                                                    <th class="sort" data-sort="description">Description</th>
-                                                    <th class="sort" data-sort="logo">Logo</th>
-                                                    <th class="sort" data-sort="phone">Created at</th>
+                                                    <th>Title</th>
+                                                    <th>Description</th>
+                                                    <th>Logo</th>
+                                                    <th>Created at</th>
 
-                                                    <th class="sort" data-sort="status">Status</th>
-                                                    <th class="sort" data-sort="action">Action</th>
+                                                    <th>Status</th>
+                                                    <th>Action</th>
                                                 </tr>
                                             </thead>
-                                            <tbody class="list form-check-all">
+                                            <tbody>
                                                 @foreach ($brands as $brand)
                                                     <tr>
-                                                        <th scope="row">
-                                                            <div class="form-check">
-                                                                <input class="form-check-input" type="checkbox"
-                                                                    name="chk_child" value="option1">
-                                                            </div>
-                                                        </th>
-                                                        <td class="id" style="display:none;"><a
-                                                                href="javascript:void(0);"
-                                                                class="fw-medium link-primary">#VZ2101</a></td>
                                                         <td class="customer_name">{{ $brand->name }}</td>
                                                         <td class="email">{{ $brand->description }}</td>
                                                         <td class="logo"><img style="height: 50px;width:50px;"
@@ -119,31 +89,10 @@
                                                     @include('admin.brand.edit')
                                                     @include('admin.brand.delete-modal')
                                                 @endforeach
-
                                             </tbody>
                                         </table>
-                                        <div class="noresult" style="display: none">
-                                            <div class="text-center">
-                                                <lord-icon src="https://cdn.lordicon.com/msoeawqm.json" trigger="loop"
-                                                    colors="primary:#121331,secondary:#08a88a"
-                                                    style="width:75px;height:75px"></lord-icon>
-                                                <h5 class="mt-2">Sorry! No Result Found</h5>
-                                                {{-- <p class="text-muted mb-0">We've searched more than 150+ Orders We did not find any orders for you search.</p> --}}
-                                            </div>
-                                        </div>
                                     </div>
 
-                                    <div class="d-flex justify-content-end">
-                                        <div class="pagination-wrap hstack gap-2">
-                                            <a class="page-item pagination-prev disabled" href="javascript:void(0);">
-                                                Previous
-                                            </a>
-                                            <ul class="pagination listjs-pagination mb-0"></ul>
-                                            <a class="page-item pagination-next" href="javascript:void(0);">
-                                                Next
-                                            </a>
-                                        </div>
-                                    </div>
                                 </div>
                             </div><!-- end card -->
                         </div>
@@ -157,42 +106,3 @@
 
         @include('admin.brand.create')
     @endsection
-    @push('js')
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js"></script>
-
-        <script type="text/javascript">
-            $('.show_confirm').click(function(event) {
-
-                var form = $(this).closest("form");
-
-                var name = $(this).data("name");
-
-                event.preventDefault();
-
-                swal({
-
-                        title: `Are you sure you want to delete this record?`,
-
-                        text: "If you delete this, it will be gone forever.",
-
-                        icon: "warning",
-
-                        buttons: true,
-
-                        dangerMode: true,
-
-                    })
-
-                    .then((willDelete) => {
-
-                        if (willDelete) {
-
-                            form.submit();
-
-                        }
-
-                    });
-
-            });
-        </script>
-    @endpush

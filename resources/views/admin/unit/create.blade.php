41f0ed149ec6ded964902bdcbcfdb2c959fa5671
dipto <diptoguho4@gmail.com>
2025-04-01 16:30:35 +0600

    unit

--- resources/views/admin/unit/create.blade.php
+++ resources/views/admin/unit/create.blade.php
@@ -4,7 +4,7 @@
          <div class="modal-content">
              <div class="modal-header">
                  <h5 class="modal-title" id="addUnitModalLabel">
-                     Add New unit
+                     Add New Unit
                  </h5>
                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
              </div>
--- resources/views/admin/unit/index.blade.php
+++ resources/views/admin/unit/index.blade.php
@@ -3,9 +3,9 @@
     <div class="card mb-4">
         <!-- Add this button to your card header (just below the <h5> tag) -->
         <div class="card-header d-flex justify-content-between align-items-center">
-            <h5 class="mb-0">unit</h5>
+            <h5 class="mb-0">Unit</h5>
             <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addUnitModal">
-                <i class="bi bi-plus-lg me-2"></i>Add unit
+                <i class="bi bi-plus-lg me-2"></i>Add Unit
             </button>
         </div>
 
